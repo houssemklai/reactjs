96d0ee4841c7286f2f382612cd543dfe4a85bc8e
unknown <23160453hk@gmail.com>
2021-12-08 15:19:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './style.css';
+import Projet from './projet.js'
+function App() {
+  return (
+    <div className="App">
+      <Projet/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projet.js
@@ -0,0 +1,25 @@
+    import hous from './ph1.jpg';
+    
+    
+    function Projet(){
+    return(
+       
+        <div>
+        <h1 className='titlered'>Your name here</h1>
+        <br/> 
+        <img src={hous} alt = "hk" />
+        <br/>
+        <img src="/ph2.jpg" alt = "hk" />
+        <div style={{ border:"solid 1px black", maxwidth:"100vw"}}></div>
+        
+<video width="320" height="240" controls>
+
+<source src="houssem real.mp4" type="video/mp4" />
+</video>
+</div>
+
+)}
+
+
+    export default Projet 
+
